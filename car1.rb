2a15dc4ce0eb9866a443c4d264202f73b9645046
Artem Suchov <artem.suchov@gmail.com>
2013-01-30 22:53:52 +0200

    some

--- car1.rb
+++ car1.rb
@@ -1,46 +1,59 @@
 # encoding: utf-8
 class Car
 	
-	attr_accessor :engine, :size
+	attr_accessor :engine, :size, :turbo || false
 
 	def initialize(opt={})
 		opt.each_with_index do |(attr,val),index|
-			send("#{attr}=", val)
+			if @engine=="" || @size == ""
+				raise "WTF? What do you put there?"
+			else
+				send("#{attr}=", val)
+			end
 		end
 	end
 
 	def engine
 		puts "#{@engine}"
 	end
-
 	def size
 		puts "#{@size}"
 	end
 
-	def engine_info
-		puts "#{@size} #{@engine.capitalize} engine"
-	end
 
-	#def initialize #to Car.new
+	def engine_info
+			if @engine.nil? || @size.nil?
+      	raise "Not enough parameters"
+
+ 			elsif @turbo == true
+ 				puts "Turbo #{@engine} #{@size.to_f} #{@turbo}"
+
+      else
+	 			puts "#{@size} #{@engine.capitalize} engine #{@turbo}"
+	  	end
+	end
 end
 
 
 a = Car.new; a.engine = :disel; a.size = 1.7
 a.engine #=> disel
 a.size #=>1.6
-a.engine_info #=>"1.6 disel engine"
-
+a.engine_info #=>"1.6 Disel engine"
 Car.new(:engine => :gas, :size => 1.6).engine_info#=> 1.6 gas engine
 
-Car.new.engine_info #=> should fail
+#Car.new.engine_info #=> should fail
+
+#Car.new(asdasd: true) #=>should fail
+
+Car.new(engine: :disel, size: 2, turbo: true).engine_info# => "Turbo disel engine 2.0"
 =begin
-Car.new(asdasd: true) #=>should fail
-Car.new(engine: :disel, size: 2, turbo: true).engine_info # => "Turbo disel engine 2.0"
 a = Car.new do 
 	self.engine= :disel
 	self.size= 3
 end
-a.engine_info	#=> "3.0 disel engine"
+=end
+#a.engine_info	#=> "3.0 disel engine"
+=begin
 a = Car.new do
    engine :diesel
    size 3
--- car2.rb
+++ car2.rb
@@ -1,19 +1,47 @@
 # encoding: utf-8
 
-a = Car.new; a.engine = :disel; a.size = 1.6
-a.engine #=> disel
-a.size #=>1.6
-a.engine_info #=>"1.6 disel engine"
-Car.new(:engine => :gas, :size => 1.6).engine_info#=> 1.6 gas engine
-Car.new.engine_info #=> should fail
-Car.new(asdasd: true) #=>should fail
-Car.new(engine: :disel, size: 2, turbo: true).engine_info # => "Turbo disel engine 2.0"
-a = Car.new do 
-	self.engine= :disel
-	self.size= 3
+class Car
+  ALLOWED = [:engine, :size, :turbo]
+
+  def initialize(args = {}, &block)
+    if block_given?
+      instance_eval &block
+    else
+      args.each { |k, v| ALLOWED.include?(k) ? instance_variable_set("@#{k}", v) : raise("the variable '@#{k}' is not allowed") }
+    end
+  end
+
+  def method_missing name, *args, &block
+    name = name.to_s
+    name.chop! if name =~ /=$/
+    if ALLOWED.include? name.to_sym
+      self.class.send(:define_method, name, proc { attr_accessor name.to_sym} )
+      instance_variable_set("@#{name}", args[0])
+    else
+      super
+    end
+  end
+
+  def engine_info
+    puts "#{'Turbo ' if @turbo}#{@size.to_f} #{@engine} engine".capitalize
+  end
 end
-a = Car.new do
-	engine :diesel
-	size 3
-end
-a.engine_info	#=> "3.0 disel engine"
+
+
+#a = Car.new; a.engine = :disel; a.size = 1.6
+#a.engine #=> disel
+#a.size #=>1.6
+#a.engine_info #=>"1.6 disel engine"
+#Car.new(:engine => :gas, :size => 1.6).engine_info#=> 1.6 gas engine
+#Car.new.engine_info #=> should fail
+#Car.new(asdasd: true) #=>should fail
+#Car.new(engine: :disel, size: 2, turbo: true).engine_info # => "Turbo disel engine 2.0"
+#a = Car.new do 
+#	self.engine= :disel
+#	self.size= 3
+#end
+#a = Car.new do
+#	engine :diesel
+#	size 3
+#end
+#a.engine_info	#=> "3.0 disel engine"
